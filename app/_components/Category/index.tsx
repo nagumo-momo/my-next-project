8f0c5ed59a387c4cdaa0b842c5d6be95fa7a2c20
nagumo-momo <nagumo656@gmail.com>
2025-06-21 09:11:01 +0900

    3章まで完了

---
+++ app/_components/Category/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+import type { Category } from "@/app/_libs/microcms";
+import styles from "./index.module.css";
+
+type Props = {
+    category: Category;
+};
+
+export default function Category({category}: Props) {
+    return (
+        <span className={styles.tag}>{category.name}</span>
+    );
+}
